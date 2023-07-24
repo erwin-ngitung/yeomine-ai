a7bbd346d10c9ab8274135a2fc4911604ade029d
Erwin Fernanda <110637602+erwin-ngitung@users.noreply.github.com>
2023-07-24 14:15:28 +0700

    Update app

--- app.py
+++ app.py
@@ -692,17 +692,8 @@ def detection(st, **state):
             except:
                 pass
     with tab3:
-        # Find your Account SID and Auth Token at twilio.com/console
-        # and set the environment variables. See http://twil.io/secure
-        account_sid = os.environ['TWILIO_ACCOUNT_SID']
-        auth_token = os.environ['TWILIO_AUTH_TOKEN']
-        client = Client(account_sid, auth_token)
-
-        token = client.tokens.create()
-
         webrtc_ctx = webrtc_streamer(key="WYH",
                                      mode=WebRtcMode.SENDRECV,
-                                     rtc_configuration={"iceServers": token.ice_servers},
                                      media_stream_constraints={"video": True, "audio": False},
                                      video_frame_callback=cs.recv,
                                      async_processing=True)
