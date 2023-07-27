5371863bf1179377ec4495376eea0f821f69c080
Erwin Fernanda <110637602+erwin-ngitung@users.noreply.github.com>
2023-07-27 14:20:27 +0700

    Update app

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -30,9 +30,14 @@ st.markdown(
     Yeomine is a product that is built by a web and desktop application using python language as backend and streamlit
      as framework. This is integrated with computer vision technology using YoloV8 Model that is developed 
      with thousands of actual and valid open coal mining data.
+    </div>
+    """, unsafe_allow_html=True)
+
+st.markdown(
+    """
     ### Want to learn more and purchase it?
     - Check out [Yeomine Landing Page] (https://erwin-ngitung.github.io/yeomine-ai/)
     - Jump into our [documentation] (https://erwin-ngitung.github.io/yeomine-ai/#gallery)
     - Ask a question in our [company] (yeomine-digital-services@gmail.com)
-    </div>
-    """, unsafe_allow_html=True)
+    """
+)
