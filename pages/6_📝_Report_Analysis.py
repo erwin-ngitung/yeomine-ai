1414c42bd0f90d6ec3087876af23471377e0f71d
Erwin Fernanda <fernandaerwin277@gmail.com>
2023-08-08 11:00:28 +0700

    Update code

--- pages/6_📝_Report_Analysis.py
+++ pages/6_📝_Report_Analysis.py
@@ -46,34 +46,34 @@ else:
     tab1, tab2 = st.tabs(['🎦 Video', '📷 Image'])
 
     with tab1:
-        # try:
-        kind_file = 'videos'
-        kind_object = state['object-videos']
-        kind_model = state['model-videos']
-        path_folder = f'{PATH}/detections/{kind_file}/{path_object[kind_object]}/annotations'
+        try:
+            kind_file = 'videos'
+            kind_object = state['object-videos']
+            kind_model = state['model-videos']
+            path_folder = f'{PATH}/detections/{kind_file}/{path_object[kind_object]}/annotations'
 
-        model = YOLO(kind_model)
+            model = YOLO(kind_model)
 
-        dataset = pd.DataFrame(columns=['Label', 'X', 'Y', 'Weight', 'Height'])
-        for file in os.listdir(path_folder):
-            data = pd.read_fwf(f'{path_folder}/{file}',
-                               names=['Label', 'X', 'Y', 'Weight', 'Height'])
+            dataset = pd.DataFrame(columns=['Label', 'X', 'Y', 'Weight', 'Height'])
+            for file in os.listdir(path_folder):
+                data = pd.read_fwf(f'{path_folder}/{file}',
+                                   names=['Label', 'X', 'Y', 'Weight', 'Height'])
 
-            dataset = pd.concat([dataset, data])
+                dataset = pd.concat([dataset, data])
 
-        dataset.dropna(inplace=True)
-        dataset['Label'] = dataset['Label'].astype(int).replace(model.names)
-        dataset['X'] = dataset['X'].astype(float)
-        dataset['Y'] = dataset['X'].astype(float)
-        dataset['Weight'] = dataset['Weight'].astype(float)
-        dataset['Height'] = dataset['Height'].astype(float)
-        dataset = dataset.reset_index(drop=True)
-        dataset['ID'] = cs.count_label(dataset)
+            dataset.dropna(inplace=True)
+            dataset['Label'] = dataset['Label'].astype(int).replace(model.names)
+            dataset['X'] = dataset['X'].astype(float)
+            dataset['Y'] = dataset['X'].astype(float)
+            dataset['Weight'] = dataset['Weight'].astype(float)
+            dataset['Height'] = dataset['Height'].astype(float)
+            dataset = dataset.reset_index(drop=True)
+            dataset['ID'] = cs.count_label(dataset)
 
-        st.table(dataset)
+            st.table(dataset)
 
-        # except (Exception,):
-        #     st.error('Please go to the menu Detection (sub-menu video) first!', icon='❎')
+        except (Exception,):
+            st.error('Please go to the menu Detection (sub-menu video) first!', icon='❎')
 
     with tab2:
         try:
@@ -81,6 +81,27 @@ else:
             kind_object = state['object-pictures']
             kind_model = state['model-pictures']
             path_folder = f'{PATH}/detections/{kind_file}/{path_object[kind_object]}/annotations'
+
+            model = YOLO(kind_model)
+
+            dataset = pd.DataFrame(columns=['Label', 'X', 'Y', 'Weight', 'Height'])
+            
+            for file in os.listdir(path_folder):
+                data = pd.read_fwf(f'{path_folder}/{file}',
+                                   names=['Label', 'X', 'Y', 'Weight', 'Height'])
+
+                dataset = pd.concat([dataset, data])
+
+            dataset.dropna(inplace=True)
+            dataset['Label'] = dataset['Label'].astype(int).replace(model.names)
+            dataset['X'] = dataset['X'].astype(float)
+            dataset['Y'] = dataset['X'].astype(float)
+            dataset['Weight'] = dataset['Weight'].astype(float)
+            dataset['Height'] = dataset['Height'].astype(float)
+            dataset = dataset.reset_index(drop=True)
+            dataset['ID'] = cs.count_label(dataset)
+
+            st.table(dataset)
 
         except (Exception,):
             st.error('Please go to the menu Detection (sub-menu picture) first!', icon='❎')
