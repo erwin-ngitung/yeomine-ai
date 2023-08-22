7c9696e388656919a30051cad21283299b5f4f38
Erwin Fernanda <fernandaerwin277@gmail.com>
2023-08-22 14:09:02 +0700

    Update code

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -24,20 +24,15 @@ with st2:
 
 st.write("# Welcome to Yeomine! 👋")
 
-st.markdown(
-    """
-    <div style="text-align: justify;">
-    Yeomine is a product that is built by a web and desktop application using python language as backend and streamlit
-     as framework. This is integrated with computer vision technology using YoloV8 Model that is developed 
-     with thousands of actual and valid open coal mining data.
-    </div>
-    """, unsafe_allow_html=True)
+st.image('data/images/brochure_1.png')
+
+st.image('data/images/brochure_2.png')
 
 st.markdown(
     """
     ### Want to learn more and purchase it?
-    - Check out [Yeomine Landing Page] (https://erwin-ngitung.github.io/yeomine-ai/)
-    - Jump into our [documentation] (https://erwin-ngitung.github.io/yeomine-ai/#gallery)
-    - Ask a question in our [company] (yeomine-digital-services@gmail.com)
+    - Check out [Yeomine Website] (https://erwin-ngitung.github.io/yeomine-ai/)
+    - Jump into our [Documentation] (https://erwin-ngitung.github.io/yeomine-ai/#gallery)
+    - Ask a question in our [Company] (yeomine-digital-services@gmail.com)
     """
 )
